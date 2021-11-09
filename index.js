a443ca0248261b0982928596fec75d1217cc12ad
Adam <a.goodkind@gmail.com>
2021-11-09 10:29:43 -0600

    add comments, remove some socket dict items

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Load the necessary libraries. This server code is hosted on Heroku at 
 // keystrokes-collablab.herokuapp.com.
+// When running on ec2, no need to worry about Heroku
 
 const express = require('express');
 const app = express();
@@ -23,7 +24,6 @@ app.get("", (req, res) => {
     res.send("connected");
   });
 
-
 http.listen(PORT, () => {
     console.log(`listening on *:${PORT}`);
 });
@@ -44,28 +44,20 @@ app.get("/reset", (req, res) => {
 })
 
 io.on('connection', (socket) => { 
-    // time-stamp at beginning of experiment
-    const d = new Date();  
-    const expDate = d.toLocaleDateString().replace(/\//g,'-'); // replace all /'s with -'s
-    const expTime = d.toLocaleTimeString('en-GB'); //24-hour time format
-    const expNode = expDate+`_`+expTime;
-
     socket.join(`room${room}`);
     // this is still updating every time a new subj joins
-    socket.emit('connection', {room: room, count: count, experimentNode: expNode});
-    // io.to(`room${room}`).emit('readyForTimer', {room: room, count: count, expDT: expDate});
-    // io.to(`room${room}`).emit("readyForTimer", data.room);
+    socket.emit('connection', {room: room, count: count});
     
     // Use a dictionary to keep track of how many people are in what room
+    // when 2nd subject joins (count=1), emit a message to App.js that will advance
+    // the prompt
     roomMap[socket.id] = count;
     if (count == 0) {
         count = count + 1;
     } else {
-        io.to(`room${room}`).emit('readyForTimer', {test: "test", room: room, count: count, experimentNode: expNode});
-        // socket.emit('readyForTimer', {room: room, count: count, expDT: expDate});
+        io.to(`room${room}`).emit('readyForTimer', {room: room, count: count});
         count = 0;
         room++;
-        
     }
     console.log(`new client connected, and the socket id was ${socket.id}`);
 
