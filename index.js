5e1cba585c180e50d0fcaab116c727a902c1303a
Adam <a.goodkind@gmail.com>
2021-11-23 15:09:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -88,9 +88,10 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log('count: ',count,'room: ',room)
         console.log('disconnect');
-        if (count == 1) {
+        if (count == 0) {
             count = 0;
             room++;
+            console.log('newcount: ',count,'newroom: ',room)
         }
 
     })
